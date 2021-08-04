0248f466dff65d7457a797e80454dc73a54e1080
Prince Patel <prince.patel.14@icloud.com>
2021-08-04 10:50:16 +0530

    Sort laganiyee 1

--- src/lib/Sorting/Sorting.cc
+++ src/lib/Sorting/Sorting.cc
@@ -2,12 +2,19 @@
 
 std::vector<int> Sorting::getBubbleSortingMessage(std::vector<int> input_Vec){
 	int n = input_Vec.size();
-
+    
+    bool swapped = false;
+    
 	for(int i = 0; i < n - 1; i++){
+	    swapped = false;
 		for(int j = 0; j < n - i - 1; j++){
 
 			if(input_Vec[j] > input_Vec[j+1]){
+			    swapped = true;
 				std::swap(input_Vec[j], input_Vec[j+1]);
+			}
+			if(!swapped){
+			    break;
 			}
 		}
 	}
--- src/main/Sorting/main.cc
+++ src/main/Sorting/main.cc
@@ -2,7 +2,6 @@
 #include "src/lib/Sorting/Sorting.h"
 
 int main(int argc, char** argv){
-    Sorting *greet = new Sorting();
     return EXIT_SUCCESS;
 
 }
