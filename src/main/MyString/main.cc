93b6afabb560cfb5854642a7653a56baf6035442
Prince Patel <prince.patel.14@icloud.com>
2021-08-07 11:21:02 +0530

    Added main test and gpg keyring

--- src/main/MyString/main.cc
+++ src/main/MyString/main.cc
@@ -4,7 +4,15 @@
 int main(int argc, char** argv){
     char testArray[1000];
 	std::cin.getline(testArray, 1000);
-    std::cout<<testArray<<std::endl;
+
+    MyString *out = new MyString();
+    std::string actual = out->getMyStringRouteMessage(testArray);
+	int actual1 = out->getMyStringCount(testArray);
+
+    std::cout << actual1 << std::endl;
+    
+    std::cout << actual << std::endl;
+    
     return EXIT_SUCCESS;
 
 }
