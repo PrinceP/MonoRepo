162c7ca84a11bf43767a24d51e1662eea57f0e3a
Prince Patel <prince.patel.14@icloud.com>
2021-08-10 10:22:10 +0530

    Clear last bit please

--- src/main/MyBit/main.cc
+++ src/main/MyBit/main.cc
@@ -1,28 +1,52 @@
 #include <iostream>
 #include "src/lib/MyBit/MyBit.h"
 
-
+//10101
+//00001 << i
+//00100 &
+//00100
 int getithBit(int n, int i){
     int mask = 1<<i;
     return (n&mask) > 0 ? 1 : 0;
 }
 
+//10101
+//00001 << i
+//00100 ~
+//11011 &
+//10001
 void clearithBit(int &n, int i){
     int mask = ~(1<<i);
     n = n&mask;
 }
 
+//10101
+//00001 << i
+//00100 |
+//10101
 void setithBit(int &n, int i){
     int mask = (1<<i);
     n = n|mask;
 }
 
+//10101
+//10_01
+//00v00 &
+//10v01
 void updateithBit(int &n, int i, int v){
     clearithBit(n, i);
     int mask = (v<<i);
     n = n|mask;
 }
 
+//10101
+//11111 << i
+//11000 &
+//10000
+void clearlastithBit(int &n, int i){
+    int mask = (-1<<i);
+    n = n&mask;
+}
 
 int main(int argc, char** argv){
     MyBit *greet = new MyBit();
@@ -55,6 +79,8 @@ int main(int argc, char** argv){
     updateithBit(n,i,v);
     std::cout << "Update ith bit = " << getithBit(n,i) << std::endl;
 
+    clearlastithBit(n,i);
+    std::cout << "Clear last i bits = " << getithBit(n,i) << std::endl;
 
     return EXIT_SUCCESS;
 
