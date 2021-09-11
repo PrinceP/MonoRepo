685c3928a60dc527142e1e4498e8f5f1df2ebe78
Prince Patel <prince.patel.14@icloud.com>
2021-09-11 16:53:44 +0530

    Heap STL, max and min

--- src/main/Heap/main.cc
+++ src/main/Heap/main.cc
@@ -1,13 +1,22 @@
 #include <iostream>
 #include "src/lib/Heap/Heap.h"
 
+class Compare{
+
+    public:
+    bool operator()(int a, int b){
+        return a<b; // Max heap and if > then min heap
+    }
+
+};
+
 int main(int argc, char** argv){
     Heap *greet = new Heap();
     std::cout << greet->getHeapMessage() << std::endl;
 
     int arr[] = {23, 21,41, 52, 64, 74, 100};
-    
-    std::priority_queue<int> pque;
+
+    std::priority_queue<int, std::vector<int>, Compare > pque;
     for(int x: arr){
         pque.push(x);
     }
