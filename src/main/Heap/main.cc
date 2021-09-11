eaa73ea4ec4c1923b6db52950b73f9aaabde2d59
Prince Patel <prince.patel.14@icloud.com>
2021-09-11 16:49:14 +0530

    Heap stl

--- src/main/Heap/main.cc
+++ src/main/Heap/main.cc
@@ -5,6 +5,16 @@ int main(int argc, char** argv){
     Heap *greet = new Heap();
     std::cout << greet->getHeapMessage() << std::endl;
 
+    int arr[] = {23, 21,41, 52, 64, 74, 100};
+    
+    std::priority_queue<int> pque;
+    for(int x: arr){
+        pque.push(x);
+    }
+    while(!pque.empty()){
+        std::cout << pque.top() << " ";
+        pque.pop();
+    }
     return EXIT_SUCCESS;
 
 }
