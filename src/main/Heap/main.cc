d4ae9d6271507fc7d297dcaf3001647cb39f3139
Prince Patel <prince.patel.14@icloud.com>
2021-09-12 21:23:37 +0530

    Heap get top K objects

--- src/main/Heap/main.cc
+++ src/main/Heap/main.cc
@@ -24,6 +24,62 @@ int maxProduct(std::vector<int> nums) {
     
 }
 
+//Car compare problem
+class Car{
+
+    std::string id;
+    int x, y;
+    public:
+    Car(std::string id, int x, int y){
+        this->id = id;
+        this->x = x;
+        this->y = y;
+    }
+    int getDist() const{
+        return x*x + y*y;
+    }
+    std::string getId(){
+        return id;
+    }
+};
+
+class CarCompare{
+    public:
+    bool operator()(const Car A, const Car B){
+       return A.getDist() < B.getDist();
+    }
+};
+
+void printKNearCars(std::vector<Car> cars, int k){
+
+    //Build a priority queue
+    std::priority_queue<Car, std::vector<Car>, CarCompare> Carheap(cars.begin(), cars.begin() + k);
+
+    //Adjust queue according to input
+    for(int i = k; i < cars.size(); i++){
+        auto car = cars[i];
+
+        //Replace the heap top if small
+        if(car.getDist() < Carheap.top().getDist()){
+            Carheap.pop();
+            Carheap.push(car);
+        }
+    }
+    std::vector<Car> nearestCars;
+    while(!Carheap.empty()){
+        nearestCars.push_back(Carheap.top());
+        Carheap.pop();
+    }
+
+    std::reverse(nearestCars.begin(), nearestCars.end());
+
+    for(auto car: nearestCars){
+        std::cout << car.getId() << std::endl;
+    }
+
+}
+
+//
 
 int main(int argc, char** argv){
     Heap *greet = new Heap();
@@ -39,6 +95,9 @@ int main(int argc, char** argv){
         std::cout << pque.top() << " ";
         pque.pop();
     }
+
+
+
     return EXIT_SUCCESS;
 
 }
