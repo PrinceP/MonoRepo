20b359a862c1f853b0511f37def38a8a8f5e45ad
Prince Patel <prince.patel.14@icloud.com>
2021-09-27 22:24:30 +0530

    Tries partial

--- src/lib/Tries/Tries.h
+++ src/lib/Tries/Tries.h
@@ -2,9 +2,35 @@
 #define TEMPLATE_GREETING_H
 
 #include <string>
+#include <unordered_map>
+
+class Node{
+
+    char data;
+    bool isTerminal;
+    std::unordered_map<char, Node*> m;
+
+    public:
+    
+    Node(char data){
+        this->data = data;
+        isTerminal = false;
+    }
+
+};
+
 
 class Tries {
+
+    Node *root;
     public:
+        Tries(){
+            root = new Node('/0');
+        }
+
+        void insert(std::string word){
+            
+        }
         std::string getTriesMessage();
 };
 
