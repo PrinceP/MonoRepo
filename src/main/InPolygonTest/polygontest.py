3e3d45a2fd2e584ecdd7897a75a6191a6c664fef
Prince Patel <prince.patel.14@icloud.com>
2021-10-02 10:52:51 +0530

    Polygon test timings

--- src/main/InPolygonTest/polygontest.py
+++ src/main/InPolygonTest/polygontest.py
@@ -2,6 +2,7 @@ import numpy as np
 import shapely
 from shapely.geometry import Polygon,MultiPoint  #Polygon
 import cv2
+import time
 
 image = cv2.imread('/Users/prince/Work/Developer/DS_Algo/src/main/InPolygonTest/input/998d98e.jpg')
 image = cv2.resize(image, (640,480))
@@ -19,20 +20,29 @@ b=np.array(line2).reshape(-1,2)
 poly2 = Polygon(b).convex_hull
 print(Polygon(b).convex_hull)
 
+# union_poly = np.concatenate((a,b))   #Merge two box coordinates to become 8*2
+# #print(union_poly)
+# print(MultiPoint(union_poly).convex_hull)
+
 intersect_polygon = None
+start_time = time.time()
 if not poly1.intersects(poly2): #If the two quadrilaterals do not intersect
+    print(time.time() - start_time)
     iou = 0
 else:
+    print(time.time() - start_time)
     try:
+        start_time = time.time()
         intersect = poly1.intersection(poly2) #intersection area
+        print(time.time() - start_time)
         intersect_polygon = [int_coords(intersect.exterior.coords)]
-        inter_area = poly1.intersection(poly2).area #intersection area
+        inter_area = intersect.area #intersection area
         iou=float(inter_area)/poly1.area
     except shapely.geos.TopologicalError:
         print('shapely.geos.TopologicalError occured, iou set to 0')
         iou = 0
  
-print(iou)
+print("Area = {} ".format(iou))
 
 isClosed = True  
 image = cv2.polylines(image, [a], 
