16167ed1c43e4f4cb3306006ba50a0d25e95aceb
Prince Patel <prince.patel.14@icloud.com>
2022-06-12 19:52:17 +0530

    cplusplus 🤌🏻

--- src/main/CPlusPlus/main.cc
+++ src/main/CPlusPlus/main.cc
@@ -41,5 +41,44 @@ int main(int argc, char** argv){
       }
       std::cout << std::endl;
     }
+
+    /*   1 */
+    /*  232 */
+    /* 34543 */
+    /*4567654 */
+
+
+    int number = 1;
+    for(int i = 1; i <= n; i++){
+      number = i; 
+      for(int j = 1; j <= n - i; j++){
+        std::cout << " ";
+      }
+
+      for(int k = 1; k <= i; k++){
+        std::cout << number;
+        number = number + 1;
+      }
+      
+      int start_number = number - 2;
+      for(int l = 1; l <= i-1; l++){
+        std::cout << start_number;
+        start_number = start_number - 1;
+      }
+
+      std::cout << std::endl;
+    }
+
+    int ans = 0;    
+    int N = n;
+    int p = 1;
+    while(N != 0){
+      int last_digit = N % 10;
+      ans = ans + last_digit*p;
+      N = N / 10;
+      p = p * 2;
+    }
+    std::cout << "Binary to decimal = " << ans;
+
     return EXIT_SUCCESS;
 }
